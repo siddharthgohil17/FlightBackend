5b2a68842b3457785c29787f3f50a0709793cc99
Siddharth <siddharthgohil07@gmail.com>
2023-09-09 16:14:43 +0530

    create database using sequelizer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const {PORT}=require('./config/serverConfig.js')
+const bodyParser=require('body-parser');
 
 
 const setUpAndStartServer=async ()=>{
     const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log(`the server run on port ${PORT} `)
     })
